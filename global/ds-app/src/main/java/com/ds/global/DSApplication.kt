8237e5399e862a44c20c9b5d2f41080b3aee1fbd
zhaozuyuan <zhaozuyuan@kuaishou.com>
2021-01-27 22:05:00 +0800

    修复app初始化的一个坑

--- global/ds-app/src/main/java/com/ds/global/DSApplication.kt
+++ global/ds-app/src/main/java/com/ds/global/DSApplication.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.ViewTreeObserver
 import com.four.app_init_handler.AppInitInjector
 import com.four.app_init_handler.StringConstant
 import com.four.app_init_handler.api.IAppLife
@@ -22,31 +23,20 @@ class DSApplication : Application() {
 
     private val appLifeListener: IAppLife = AppInitInjector
 
-    private var firstFrameShowed = false
     private val handler = Handler(Looper.getMainLooper())
 
     private val activityCallback = object : ActivityLifecycleCallbacks {
         override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-            tryRegisterFirstFrameListener(activity)
+            if (tryRegisterFirstFrameListener(activity)) {
+                unregisterActivityLifecycleCallbacks(this)
+            }
         }
-
-        override fun onActivityStarted(activity: Activity) {
-        }
-
-        override fun onActivityResumed(activity: Activity) {
-        }
-
-        override fun onActivityPaused(activity: Activity) {
-        }
-
-        override fun onActivityStopped(activity: Activity) {
-        }
-
-        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-        }
-
-        override fun onActivityDestroyed(activity: Activity) {
-        }
+        override fun onActivityStarted(activity: Activity) { }
+        override fun onActivityResumed(activity: Activity) { }
+        override fun onActivityPaused(activity: Activity) { }
+        override fun onActivityStopped(activity: Activity) { }
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) { }
+        override fun onActivityDestroyed(activity: Activity) { }
     }
 
     init {
@@ -66,15 +56,20 @@ class DSApplication : Application() {
         appLifeListener.onCreate(instance)
     }
 
-    private fun tryRegisterFirstFrameListener(activity: Activity) {
-        if (!firstFrameShowed && activity is IFirstFramePage) {
-            activity.window.decorView.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
-                if (!firstFrameShowed) {
-                    firstFrameShowed = true
-                    appLifeListener.afterFirstFrame(instance)
-                    handler.postDelayed({ appLifeListener.afterFirstFrame3S(instance) }, 3000)
+    private fun tryRegisterFirstFrameListener(activity: Activity): Boolean {
+        return if (activity is IFirstFramePage) {
+            val observer = activity.window.decorView.viewTreeObserver
+            observer.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    val duration = 3000L
+                    handler.post { appLifeListener.afterFirstFrame(instance) }
+                    handler.postDelayed({ appLifeListener.afterFirstFrame3S(instance) }, duration)
+                    activity.window.decorView.viewTreeObserver.removeOnGlobalLayoutListener(this)
                 }
-            }
+            })
+            true
+        } else {
+            false
         }
     }
 }
